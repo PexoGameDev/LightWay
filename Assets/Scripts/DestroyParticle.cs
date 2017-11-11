f73207dc0c27f15baf0163a40f8a033547717887
Daniel Budziński <artuditu74@gmail.com>
2017-11-11 16:13:36 +0100

    Added Camera Follow and Particle Destroy

---
+++ Assets/Scripts/DestroyParticle.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class DestroyParticle : MonoBehaviour
+{
+    public float duration = 5;
+
+	void Start (){ Invoke("Destroy", duration); }
+
+    void Destroy(){ Destroy(gameObject); }
+}
