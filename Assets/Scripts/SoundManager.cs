4008b5493b93477360f7db415cb704c81df81d24
Daniel Budziński <artuditu74@gmail.com>
2017-11-14 20:19:07 +0100

    Added Sound Manager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    [System.Serializable]
+    public struct Sound
+    {
+        public string name;
+        public AudioSource source;
+
+        public Sound(string name, AudioSource source)
+        {
+            this.name = name;
+            this.source = source;
+        }
+    }
+
+    public Sound[] soundList;
+
+    [Range(0, 1)]
+    public float pitchMin;
+    [Range(0, 1)]
+    public float pitchMax;
+    [Range(0, 1)]
+    public float volumeMin;
+    [Range(0, 1)]
+    public float volumeMax;
+
+    private void Start()
+    {
+        PlaySound("Sho 2");
+    }
+
+    public void PlaySound (string name)
+    {
+        foreach(Sound sound in soundList)
+        {
+            if(sound.name == name)
+            {
+                sound.source.pitch = Random.Range(pitchMin, pitchMax);
+                sound.source.volume = Random.Range(volumeMin, volumeMax);
+
+                sound.source.Play();
+
+                break;
+            }
+        }
+	}
+}
