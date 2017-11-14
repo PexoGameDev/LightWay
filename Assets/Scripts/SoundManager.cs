9c78bbba98c929cec650a1304c97da782f56dc8c
Daniel Budziński <artuditu74@gmail.com>
2017-11-14 20:27:36 +0100

    SoundManager update

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -10,23 +10,29 @@ public class SoundManager : MonoBehaviour {
         public string name;
         public AudioSource source;
 
-        public Sound(string name, AudioSource source)
+        [Range(-3, 3)]
+        public float pitchMin;
+        [Range(-3, 3)]
+        public float pitchMax;
+        [Range(0, 1)]
+        public float volumeMin;
+        [Range(0, 1)]
+        public float volumeMax;
+
+        public Sound(string name, AudioSource source, float pitchMin, float pitchMax, float volumeMin, float volumeMax)
         {
             this.name = name;
             this.source = source;
+            this.pitchMin = pitchMin;
+            this.pitchMax = pitchMax;
+            this.volumeMin = volumeMin;
+            this.volumeMax = volumeMax;
         }
     }
 
     public Sound[] soundList;
 
-    [Range(0, 1)]
-    public float pitchMin;
-    [Range(0, 1)]
-    public float pitchMax;
-    [Range(0, 1)]
-    public float volumeMin;
-    [Range(0, 1)]
-    public float volumeMax;
+
 
     private void Start()
     {
@@ -39,8 +45,8 @@ public class SoundManager : MonoBehaviour {
         {
             if(sound.name == name)
             {
-                sound.source.pitch = Random.Range(pitchMin, pitchMax);
-                sound.source.volume = Random.Range(volumeMin, volumeMax);
+                sound.source.pitch = Random.Range(sound.pitchMin, sound.pitchMax);
+                sound.source.volume = Random.Range(sound.volumeMin, sound.volumeMax);
 
                 sound.source.Play();
 
