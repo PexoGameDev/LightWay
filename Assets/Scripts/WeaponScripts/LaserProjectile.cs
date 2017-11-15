7ce199d1c3cbcf43a867330cf8a3ee8ab1fb9897
Pexo <pexo.yt.pl@gmail.com>
2017-11-15 11:15:58 +0100

    Fixed debug buttons - they do not invoke movement anymore, did some tweaking with laser, it still doesn't work tho

---
+++ Assets/Scripts/WeaponScripts/LaserProjectile.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class LaserProjectile : MonoBehaviour {
+
+    #region Variables
+    // FIELDS //
+
+    // PUBLIC PROPERTIES //
+
+    // PRIVATE PROPERTIES //
+
+    #endregion
+
+    #region Unity Methods
+    void Start () 
+	{
+        	
+
+	}
+	
+    void Update () 
+	{
+		
+
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Collider[] objectsInExplosionRange = Physics.OverlapBox(transform.position, new Vector3(1f, 1f, 1f)); //Working on it, it's supposed to be collider covering whole line renderer, but well... it doesn't work yet.
+        for (int i = 0; i < objectsInExplosionRange.Length; i++)
+        {
+            Enemy tmpEnemy = objectsInExplosionRange[i].GetComponent<Enemy>();
+            if (tmpEnemy != null)
+            {
+                //damage 'em
+            }
+        }
+    }
+    #endregion
+
+    #region Public Methods
+    // PUBLIC METHODS //
+
+    #endregion
+
+    #region Private Methods
+    // PRIVATE METHODS //
+
+    #endregion
+}
